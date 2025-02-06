23ae6d11a0f38afb3f9be23f7536bc438fac0ede
unknown <shirefmessi@gmail.com>
2025-02-06 14:16:52 +0200

    format the price and add it into the product and prepare the single product to show it in the separated screen

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ import { ErrorElement } from "./components";
 
 // loaders
 import { loader as landingLoader } from "./pages/Landing";
+import { loader as singleProductLoader } from "./pages/SingleProduct";
 
 // actions
 
@@ -38,6 +39,8 @@ const router = createBrowserRouter([
       {
         path: "/products/:id",
         element: <SingleProduct />,
+        errorElement: <ErrorElement />,
+        loader: singleProductLoader,
       },
       {
         path: "/cart",
