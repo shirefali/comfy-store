9488e4b8541039fbdfea5998107abdb359ec5aad
unknown <shirefmessi@gmail.com>
2025-02-07 15:10:47 +0200

    add the single product layoutand  choosing product color fanctionality

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -13,7 +13,77 @@ const SingleProduct = () => {
   const { image, title, price, description, colors, company } =
     product.attributes;
   const dollarsAmount = formatPrice(price);
-  return <div></div>;
+
+  const [productColor, setProductColor] = useState(colors[0]);
+  const [amount, setAmount] = useState(1);
+
+  const handleAmount = (e) => {
+    setAmount(parseInt(e.target.value));
+  };
+
+  return (
+    <section>
+      <div className="text-md breadcrumbs">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li to="/products">Products</li>
+        </ul>
+      </div>
+      {/* PRODUCT */}
+      <div className="mt-6 grid gap-y-8 lg:grid-cols-2 lg:gap-x-16">
+        <img
+          src={image}
+          alt={title}
+          className="w-96 h-96 object-cover rounded-lg lg:w-full m-auto"
+        />
+        <div className="text-center lg:text-start">
+          <h1 className="capitalize text-3xl font-bold">{title}</h1>
+          <h4 className="text-xl my-3 font-bold">{company}</h4>
+          <span className="text-xl">{dollarsAmount}</span>
+          <p className="mt-8 text-xl leading-8">{description}</p>
+          <div className="mt-6">
+            <h4 className="text-md tracking-wider font-medium capitalize">
+              colors
+            </h4>
+            {/* COLORS */}
+            <div className="mt-4">
+              {colors.map((color) => {
+                return (
+                  <button
+                    key={color}
+                    type="button"
+                    className={`badge h-6 w-6 mr-2 ${
+                      color === productColor && "border-[3px] border-secondary"
+                    }`}
+                    style={{ backgroundColor: `${color}` }}
+                    onClick={() => setProductColor(color)}
+                  ></button>
+                );
+              })}
+            </div>
+            {/* AMOUNT */}
+            <div className="form-control w-full max-w-xs mt-4">
+              <label className="label" htmlFor="label">
+                <h4 className="capitalize text-md font-medium tracking-wider">
+                  amount
+                </h4>
+              </label>
+              <select
+                className="select select-secondary selected-bordered select-md"
+                id="amount"
+                value={amount}
+                onChange={handleAmount}
+              >
+                <option value="1"></option>
+              </select>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default SingleProduct;
