485b508b8f82ab4985a73fd34b048cc071accc37
unknown <shirefmessi@gmail.com>
2025-02-26 15:00:39 +0200

    try to refactor the theme and add the userSlice functionality

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,9 +1,35 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
 
-const initialState = {};
+const themes = {
+  winter: "winter",
+  dracula: "dracula",
+};
+
+const getThemeFromLocalStorage = () => {
+  const theme = localStorage.getItem("theme") || themes.winter;
+};
+
+const initialState = {
+  user: { username: "shiref ali" },
+  theme: getThemeFromLocalStorage,
+};
 
 const userSlice = createSlice({
   name: "user",
   initialState,
-  reducers: {},
+  reducers: {
+    loginUser: (state, action) => {
+      console.log("login");
+    },
+    logoutUser: (state) => {
+      console.log("logout");
+    },
+    toggleTheme: (state) => {
+      console.log("toggle theme");
+    },
+  },
 });
+
+export const { loginUser, logoutUser, toggleTheme } = userSlice.actions;
+export default userSlice.reducer;
