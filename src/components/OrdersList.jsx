e914ed96fef2f22fbfb31cf046d64eb0eed71a16
unknown <shirefmessi@gmail.com>
2025-03-10 12:52:40 +0200

    working on OrdersList component

--- src/components/OrdersList.jsx
+++ src/components/OrdersList.jsx
@@ -1,5 +1,37 @@
+import { useLoaderData } from "react-router-dom";
+import day from "dayjs";
+import advancedFormat from "dayjs/plugin/advancedFormat";
+day.extend(advancedFormat);
+
 const OrdersList = () => {
-  return <h1 className="text-2xl">OrdersList</h1>;
+  const { orders, meta } = useLoaderData();
+
+  return (
+    <div>
+      <h4 className="mb-4 capitalize">
+        total orders : {meta.pagination.total}
+      </h4>
+      <div className="overflow-x-auto">
+        <table className="table table-zebra">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Address</th>
+              <th>Products</th>
+              <th>Cost</th>
+              <th className="hidden sm:block">Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((order) => {
+              const id = order.id;
+              return <tr>single order</tr>;
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default OrdersList;
--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -28,7 +28,17 @@ export const loader =
   };
 
 const Orders = () => {
-  return <h1 className="text-4xl">Orders</h1>;
+  const { meta } = useLoaderData();
+  if (meta.pagination.total < 1) {
+    return <SectionTitle text="please make an order" />;
+  }
+  return (
+    <>
+      <SectionTitle text="Your Orders" />
+      <OrdersList />
+      <PaginationContainer />
+    </>
+  );
 };
 
 export default Orders;
