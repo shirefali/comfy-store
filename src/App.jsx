c84df306bbe31c592ab0e87ea54d9316a36ee42a
unknown <shirefmessi@gmail.com>
2025-03-13 14:11:49 +0200

    add react query to the project and prepare it in some pages

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,8 @@ import {
   SingleProduct,
 } from "./pages/index";
 import { ErrorElement } from "./components";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 // loaders
 import { loader as landingLoader } from "./pages/Landing";
@@ -27,6 +29,14 @@ import { action as checkoutAction } from "./components/CheckoutForm";
 
 import { store } from "./store";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -37,19 +47,19 @@ const router = createBrowserRouter([
         index: true,
         element: <Landing />,
         errorElement: <ErrorElement />,
-        loader: landingLoader,
+        loader: landingLoader(queryClient),
       },
       {
         path: "/products",
         element: <Products />,
         errorElement: <ErrorElement />,
-        loader: productsLoader,
+        loader: productsLoader(queryClient),
       },
       {
         path: "/products/:id",
         element: <SingleProduct />,
         errorElement: <ErrorElement />,
-        loader: singleProductLoader,
+        loader: singleProductLoader(queryClient),
       },
       {
         path: "/cart",
@@ -63,12 +73,12 @@ const router = createBrowserRouter([
         path: "/checkout",
         element: <Checkout />,
         loader: checkoutLoader(store),
-        action: checkoutAction(store),
+        action: checkoutAction(store, queryClient),
       },
       {
         path: "/orders",
         element: <Orders />,
-        loader: ordersLoader(store),
+        loader: ordersLoader(store, queryClient),
       },
     ],
   },
@@ -87,7 +97,12 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />;
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,18 +2,20 @@ import { Filters, ProductsContainer, PaginationContainer } from "../components";
 import { customFetch } from "../utils";
 const url = "/products";
 
-export const loader = async ({ request }) => {
-  const params = Object.fromEntries([
-    ...new URL(request.url).searchParams.entries(),
-  ]);
+export const loader =
+  (QueryClient) =>
+  async ({ request }) => {
+    const params = Object.fromEntries([
+      ...new URL(request.url).searchParams.entries(),
+    ]);
 
-  const response = await customFetch(url, {
-    params,
-  });
-  const products = response.data.data;
-  const meta = response.data.meta;
-  return { products, meta, params };
-};
+    const response = await customFetch(url, {
+      params,
+    });
+    const products = response.data.data;
+    const meta = response.data.meta;
+    return { products, meta, params };
+  };
 
 const Products = () => {
   return (
