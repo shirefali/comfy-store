a078d27d84c2f0abf3649e1ba4af8b4cf91079c3
unknown <shirefmessi@gmail.com>
2025-03-14 15:25:55 +0200

    add react-query to Landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -3,8 +3,15 @@ import { customFetch } from "../utils";
 
 const url = "/products?featured=true";
 
+const FeaturedProductsQuery = {
+  queryKey: ["featuredProducts"],
+  queryFn: () => customFetch(url),
+};
+
 export const loader = (queryClient) => async () => {
-  const response = await customFetch(url);
+  const response = await queryClient.ensureQueryData(FeaturedProductsQuery);
+  console.log(response);
+
   const products = response.data.data;
   return { products };
 };
